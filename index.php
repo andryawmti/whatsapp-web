21f5216220ef9f48f08c9e0110fc5c4e36ce4eef
andryawmti <34875303+andryawmti@users.noreply.github.com>
2018-02-01 11:46:36 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h2 class="jumbotron">Hello Idiot!</h2>
+        <div class="row" style="text-align: center;">
+            <div class="form-group">
+                <a class="btn btn-info" href="whatsapp://send?text=Hello%2C%20World!"><i class="fa fa-whatsapp"></i> Call Me</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
